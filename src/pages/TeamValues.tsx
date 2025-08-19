0bfdd8d975224983aea81b4f74f03e0607399d24
Hadeer Salah <hadeersalah127@gmail.com>
2025-08-19 11:45:59 +0300

    Finishing the team values chart

--- src/pages/TeamValues.tsx
+++ src/pages/TeamValues.tsx
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import {
   LineChart,
   CartesianGrid,
@@ -5,10 +6,12 @@ import {
   XAxis,
   YAxis,
   Legend,
-  ResponsiveContainer
-} from 'recharts';
+  ResponsiveContainer,
+} from "recharts";
+import { DashboardContext } from "../components/DashboardContext";
 
 const TeamValues = () => {
+  const { isDark } = useContext(DashboardContext);
   const data = [
     { value: "Trust", score: 80 },
     { value: "Integrity", score: 70 },
@@ -18,28 +21,38 @@ const TeamValues = () => {
   ];
 
   return (
-    <section className='card bg-base-100 shadow-sm dark:bg-[#28264f]  min-[285px]:w-xs min-[285px]:mx-auto lg:w-120 rounded-xl py-5'>
-      <div className='card-body px-7'>
+    <section className="card bg-base-100 shadow-sm dark:bg-[#28264f]  min-[285px]:w-xs min-[285px]:mx-auto lg:w-120 rounded-xl py-5">
+      <div className="card-body px-7">
         <h2 className="card-title text-xl dark:text-white text-gray-600">
           Team Values
         </h2>
-        <hr className='py-5 text-gray-300'/>
+        <hr className="py-5 text-gray-300" />
         <div className="h-[300px] -translate-x-5">
           <ResponsiveContainer className="w-full h-full">
-          <LineChart data={data}>
-            <CartesianGrid stroke='#ccc' />
-            <Line type="monotone" dataKey="score" />
-            <XAxis dataKey="value" />
-            <YAxis />
-            <Legend />
-          </LineChart>
-        </ResponsiveContainer>
+            <LineChart data={data}>
+              <CartesianGrid stroke={isDark ? "#fff" : "#ccc"} />
+              <Line
+                type="monotone"
+                dataKey="score"
+                stroke={isDark ? "#ffff" : "#8a2cf6"}
+                dot={{ stroke: "#8a2cf6" }}
+              />
+              <XAxis
+                dataKey="value"
+                stroke={isDark ? "#fff" : "#333"}
+                tick={{ fill: isDark ? "#fff" : "#333", fontSize: 12 }}
+              />
+              <YAxis
+                stroke={isDark ? "#fff" : "#333"}
+                tick={{ fill: isDark ? "#fff" : "#333", fontSize: 12 }}
+              />
+              <Legend />
+            </LineChart>
+          </ResponsiveContainer>
         </div>
-        
-
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default TeamValues
+export default TeamValues;
