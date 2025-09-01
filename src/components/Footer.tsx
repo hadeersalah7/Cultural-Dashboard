d58b9fe0222e19d11efaed77eb605c1037b4a63e
Hadeer Salah <hadeersalah127@gmail.com>
2025-09-01 12:56:37 +0300

    Adding a favicon and updating the readme

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <div className='flex justify-center items-center text-gray-500 dark:text-white mt-10 mb-12'>
-            <h4 className='text-lg'>Cultural Dashboard © 2025 . All rights reserved.</h4>
+            <h4 className='text-lg'>Cultural Dashboard © 2025. All rights reserved.</h4>
         </div>
     )
 }
